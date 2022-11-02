ed5e49cd1caffec8c167725e2fad6f88cd48108c
Nam Tran <namtrhg@gmail.com>
2022-11-02 17:22:50 +0700

    fix: updagte

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export const MainPage = () => {
     >
       <h1
         className={clsx(
-          'text-7xl font-bold',
+          'text-4xl lg:text-7xl font-bold',
           darkMode ? 'text-white' : 'text-black',
         )}
       >
@@ -26,11 +26,11 @@ export const MainPage = () => {
       </h1>
       <h2
         className={clsx(
-          'text-4xl font-semibold',
+          'text-xl lg:text-4xl font-semibold',
           darkMode ? 'text-white' : 'text-black',
         )}
       >
-        Enthusiastic | Creative | Minimalist | Frontend developer 👨🏻‍💻
+        Enthusiastic | Creative | Minimalist | Web Developer 👨🏻‍💻
       </h2>
       <div className="flex items-center space-x-6">
         <a href="https://github.com/namtrhg" target="_blank" rel="noreferrer">
@@ -65,6 +65,77 @@ export const MainPage = () => {
             className={clsx('h-9 w-9', darkMode ? 'text-white' : 'text-black')}
           />
         </a>
+      </div>
+      <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-20 text-sm">
+        <div
+          className={clsx(
+            'text-left space-y-2',
+            darkMode ? 'text-white' : 'text-black',
+          )}
+        >
+          <p className="font-bold">Projects that I have worked</p>
+          <ul className="list-disc text-left ml-5 space-y-2">
+            <li>
+              <a
+                className="underline"
+                href="https://www.gravitylabs.co/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Gravity
+              </a>{' '}
+              - Frontend Developer
+            </li>
+            <li>
+              <a
+                className="underline"
+                href="https://mstation.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Mstation
+              </a>{' '}
+              - Frontend Developer
+            </li>
+            <li>
+              <a
+                className="underline"
+                href="https://legendfantasywar.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Legend Fantasy War
+              </a>{' '}
+              - Frontend Developer
+            </li>
+            <li>
+              <a
+                className="underline"
+                href="https://kd.newatlantic.vn/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Newatlantic ERP Software
+              </a>{' '}
+              - Fullstack Developer
+            </li>
+          </ul>
+        </div>
+        <div
+          className={clsx(
+            'text-left space-y-2',
+            darkMode ? 'text-white' : 'text-black',
+          )}
+        >
+          <p className="font-bold">Technologies that I used</p>
+          <ul className="list-disc text-left ml-5 space-y-2">
+            <li>NextJS</li>
+            <li>TailwindCSS</li>
+            <li>MySQL</li>
+            <li>SequelizeJS</li>
+            <li>NodeJS</li>
+          </ul>
+        </div>
       </div>
       <SwitchButton
         labelStyle={clsx('!mt-2', darkMode ? '!text-white' : '!text-black')}
